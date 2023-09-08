2e082b3d95ee0135906dbfcf063812d0d12e3a71
Richard Tatum <rtatum@pm.me>
2023-09-09 00:10:27 +0100

    Add username to bet return to show in logs

--- src/Bot/Models/Bet.cs
+++ src/Bot/Models/Bet.cs
@@ -3,6 +3,7 @@ namespace Dohyo.Models;
 public class Bet
 {
     public ulong UserId { get; set; }
+    public string Username { get; set; }
     public int Amount { get; set; }
     public Side Side { get; set; }
 }
